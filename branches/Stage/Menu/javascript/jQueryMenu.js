e780ffa360be9939220e30fbcabaa7ab372a24d9
nicolasrnp@gmail.com <nicolasrnp@gmail.com@70c4d950-4e09-c78e-cd16-bf2e1e36ada0>
2012-04-29 12:19:21 +0000

    maj

--- branches/Stage/Menu/javascript/jQueryMenu.js
+++ branches/Stage/Menu/javascript/jQueryMenu.js
@@ -8,6 +8,8 @@
 		//parametres par défauts
 		var defauts = {
 			'button' : '.title',
+			'menu' : '.menu',
+			'deroulable' : '.deroulable',
 			'delta' : 0,
 			'toTop' : true,
 			'css' : {
@@ -26,35 +28,32 @@
 		
 		//mise des propriétés si besoin
 		if (parametres['delta'] != 0)
-			parametres['css']['top'] = parametres['toTop'] ? - parametres['delta'] : parametres['delta']; 
+			parametres['css']['top'] = parametres['toTop'] === true ? - parametres['delta'] : parametres['delta']; 
 		
 		//attribution du css
-		$(this).css(parametres['css']);
-		$(this).find('.deroulable').css('margin', '0px');
+		$(this).find(parametres['menu']).css(parametres['css']);
+		$(this).find(parametres['deroulable']).css('margin', '0px');
 		
 		//calcul de la taille à allouer lors du déroulement
 		var taille = 0;
-		$(this).find('.deroulable').first().children().each(function(index, item)
+		$(this).find(parametres['deroulable']).first().children().each(function(index, item)
 		{
 			taille += $(item).outerHeight();
 		});
 		
 		//lors d'un clic on demande un comportement toggle pour plier et deplier le menu
-		$(parametres['button']).toggle(function(event) {
-			$(theMenu).stop().animate({height : taille,top : -parametres['delta'] - taille}, {queue : false,duration : parametres['duration']});
-			$.each(parametres['listButtonMenu'], function(index, item){
-				//alert(item + " != " + parametres['button'] + " = " + (item != parametres['button']));
-				if (item[0] != parametres['button'])
-				{
-					if($(item[1]).toggleMenuIsOpen())
-						$(item[0]).click();
-				}
+		$(this).find(parametres['button']).toggle(function(event) {
+			var top = parametres['toTop'] === true ? -parametres['delta'] - taille : parametres['delta'] + taille ;
+			$(theMenu).find(parametres['menu']).stop().animate({height : taille,top : top}, {queue : false,duration : parametres['duration']});
+			$.each(parametres['listButtonMenu'], function(index, item) {
+				if ($(item).text() != $(theMenu).text() && $(item).find(parametres['menu']).toggleMenuIsOpen())
+					$(item).find(parametres['button']).click();
 			});
 		},
 		function(event) {
-			$(theMenu).stop().animate({height : "0px",top : -parametres['delta']}, {queue : false,duration : parametres['duration']});
+			$(theMenu).find(parametres['menu']).stop().animate({height : "0px",top : -parametres['delta']}, {queue : false,duration : parametres['duration']});
 		});
-			
+		
 		return this;
 	}
 	
